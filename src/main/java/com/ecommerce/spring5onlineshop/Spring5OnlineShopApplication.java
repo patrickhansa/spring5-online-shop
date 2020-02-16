256895deb3a933b0d4558c341d630288346699ac
Hansa Patrick <patrick_hansa@yahoo.com>
2020-02-16 11:18:38 +0200

    Initial commit

---
+++ src/main/java/com/ecommerce/spring5onlineshop/Spring5OnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.spring5onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5OnlineShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5OnlineShopApplication.class, args);
+    }
+
+}
